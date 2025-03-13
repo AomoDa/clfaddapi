0dd30a1dead6bd49c4b6217be118c0f1c1b21106
Aomoda <528451599@qq.com>
2025-03-13 09:44:52 +0800

    Update vls.R

--- vls.R
+++ vls.R
@@ -4,8 +4,7 @@ library(stringr)
 
 isselect = function(tag){
 	if(is.na(tag)) return(FALSE)
-	vls = c("HKG","HK","hkg","hk","TW","tw")
-	#vls = c("TW","联通","电信","移动")
+	vls = c("HKG","HK","hkg","hk","TW","tw","KR")
 	str_cnt = sum(str_count(tag,vls))
 	if(str_cnt>0) return(TRUE)
 	return(FALSE)	
@@ -19,7 +18,7 @@ getVlessInfo <- function(txt_list){
 		info = str_extract(URLdecode(i),".+://[^@]+@([^:]+):(\\d+).+#(.+)",group=c(1,2,3))
 		if(isselect(info[3])){
 			if(any(dom==info[1])) next()
-			tmp = data.frame(ips =sprintf("%s:%s#HKG%s",info[1],info[2],ind)) 
+			tmp = data.frame(ips =sprintf("%s:%s#HK%s",info[1],info[2],ind)) 
 			rlt = rbind(rlt,tmp)
 			ind = ind + 1
 			dom = append(dom,info[1]) 
